c74e7d7382a0d3c8f498876c652905fae5596688
Mario Carrocera Menendez <46903906+MarioCarrocera@users.noreply.github.com>
2021-05-01 13:14:30 -0500

    Add files via upload

--- OnTimeDebugB.php
+++ OnTimeDebugB.php
@@ -1,11 +1,147 @@
 <?php
-trait DebugB{
+trait Debug{
 	private $DebugActive=FALSE;
 	private $DebugMode='basic';
 	private $DebugFor='system';
-	private $DebugTo='disk';
+	private $DebugTo='screen';
 	private $SaveError=FALSE;
 	public  $Debughold=array();
+	
+	function DbgStr()
+	{
+		if ($this->ot_can('owner','debug')) {
+			$this->DebugActive=TRUE;
+			$this->Debughold=[];
+		}
+		$this->ot_log( __METHOD__ , __FUNCTION__ , func_get_args() , $this->retval );
+		return $this->retval;
+	}
+	function DbgClrMmr()
+	{
+		if ($this->ot_can('owner','debug')) {
+			$this->Debughold=[];
+		}
+		$this->ot_log( __METHOD__ , __FUNCTION__ , func_get_args() , $this->retval );
+		return $this->retval;
+	}
+	function DbgStp()
+	{
+		if ($this->ot_can('owner','debug')) {
+			$this->DebugActive=FALSE;
+			$this->Debughold=[];
+		}
+		$this->ot_log( __METHOD__ , __FUNCTION__ , func_get_args() , $this->retval );
+		return $this->retval;
+	}
+	function DbgMdeBsc()
+	{
+		if ($this->ot_can('owner','debug')) {
+			$this->DebugMode='basic';
+		}
+		$this->ot_log( __METHOD__ , __FUNCTION__ , func_get_args() , $this->retval );
+		return $this->retval;
+	}
+	function DbgMdeAdv()
+	{
+		if ($this->ot_can('owner','debug')) {
+			$this->DebugMode='advance';
+		}
+		$this->ot_log( __METHOD__ , __FUNCTION__ , func_get_args() , $this->retval );
+		return $this->retval;
+	}
+	function DbgForUsr()
+	{
+		if ($this->ot_can('owner','debug')) {
+			$this->DebugFor='user';
+		}
+		$this->ot_log( __METHOD__ , __FUNCTION__ , func_get_args() , $this->retval );
+		return $this->retval;
+	}
+	function DbgForSys()
+	{
+		if ($this->ot_can('owner','debug')) {
+			$this->DebugFor='system';
+		}
+		$this->ot_log( __METHOD__ , __FUNCTION__ , func_get_args() , $this->retval );
+		return $this->retval;
+	}
+	function DbgToScr()
+	{
+		if ($this->ot_can('owner','debug')) {
+			$this->DebugTo='screen';
+		}
+		$this->ot_log( __METHOD__ , __FUNCTION__ , func_get_args() , $this->retval );
+		return $this->retval;
+	}
+	function DbgToMmr()
+	{
+		if ($this->ot_can('owner','debug')) {
+			$this->DebugTo='memory';
+		}
+		$this->ot_log( __METHOD__ , __FUNCTION__ , func_get_args() , $this->retval );
+		return $this->retval;
+	}
+	function DbgToDsk()
+	{
+		if ($this->ot_can('owner','debug')) {
+			$this->DebugTo='disk';
+		}
+		$this->ot_log( __METHOD__ , __FUNCTION__ , func_get_args() , $this->retval );
+		return $this->retval;
+	}
+	function DbgErrOn()
+	{
+		if ($this->ot_can('owner','debug')) {
+			$this->SaveError=TRUE;
+		}
+		$this->ot_log( __METHOD__ , __FUNCTION__ , func_get_args() , $this->retval );
+		return $this->retval;
+	}
+	function DbgErrOff()
+	{
+		if ($this->ot_can('owner','debug')) {
+			$this->SaveError=FALSE;
+		}
+		$this->ot_log( __METHOD__ , __FUNCTION__ , func_get_args() , $this->retval );
+		return $this->retval;
+	}
+	function DbgShw()
+	{
+		if ($this->ot_can('owner','debug')) {
+			$retval = $this->ot_getlist('debug/','log*');
+		}
+		$this->ot_log( __METHOD__ , __FUNCTION__ , func_get_args() , $retval );
+		return $retval;
+	}
+	function DbgShwLog($name)
+	{
+		if ($this->ot_can('owner','debug')) {
+			$retval = $this->ot_readif($name,'debug');
+		}
+		$this->ot_log( __METHOD__ , __FUNCTION__ , func_get_args() , $retval );
+		return $retval;
+	}
+	function DbgDltLog($name)
+	{
+		if ($this->ot_can('owner','debug')) {
+			$this->ot_deleteinside($name,'debug');
+		}
+		$this->ot_log( __METHOD__ , __FUNCTION__ , func_get_args() , $this->retval );
+		return $this->retval;
+	}
+	function DbgClrLog()
+	{
+		if ($this->ot_can('owner','debug')) {
+			$tmp=$this->DbgShw();
+			foreach ($tmp as $clave => $valor) {
+				$this->ot_deleteinside($valor,'debug');
+			}
+		}
+		$this->ot_log( __METHOD__ , __FUNCTION__ , func_get_args() , $this->retval );
+		return $this->retval;
+	}
+
+	
 	function ot_show($trac, $level=1){
 		if ($level==1){
 			echo '<br>';
@@ -34,7 +170,18 @@ trait DebugB{
 			}
 		}
 	}
+	function ot_ed($error){
+		if (array_key_exists($error, $this->errtext)) {
+			return $this->errtext[$error];
+		} else{
+			return 'Error not defined';
+		}
+	}
+	function ot_reg($in,$from,$parameters){
+		$this->dbg_addin($from,array('in'=> str_replace('::'.$from,'',$in ),'parameters'=>count ($parameters)),"OnTime_Development.tas","help");
+	}
 	function ot_func($in,$from,$parameters){
+		$this->dbg_addin($from,array('in'=> str_replace('::'.$from,'',$in ),'parameters'=>count ($parameters)),"OnTime_Development.tas","help");
 		if ($this->DebugActive){
 			if($this->DebugMode=='advance'){
 				$code = uniqid($this->actses , true );				
@@ -49,9 +196,9 @@ trait DebugB{
 				if ($this->DebugTo=='disk'){
 					$ext = date("Ymd");
 					if ($this->DebugFor=='user'){
-						$this->dbg_addin($code,$val,'log_'.$this->id.'.'.$ext,'debug');
+						$this->dbg_addin($code,$val,'log_'.$this->id.'.'.$ext);
 					} else{
-						$this->dbg_addin($code,$val,'log_system.'.$ext,'debug');	
+						$this->dbg_addin($code,$val,'log_system.'.$ext);	
 					}
 				}
 			}
@@ -60,6 +207,7 @@ trait DebugB{
 		$this->retval=FALSE;
 	}	
 	function ot_funct($in,$from,$parameters){
+		$this->dbg_addin($from,array('in'=>str_replace('::'.$from,'',$in ),'parameters'=>count ($parameters)),"OnTime_Development.tas","help");
 		if ($this->DebugActive){
 			if($this->DebugMode=='advance'){
 				$code = uniqid($this->actses , true );				
@@ -74,9 +222,9 @@ trait DebugB{
 				if ($this->DebugTo=='disk'){
 					$ext = date("Ymd");
 					if ($this->DebugFor=='user'){
-						$this->dbg_addin($code,$val,'log_'.$this->id.'.'.$ext,'debug');
+						$this->dbg_addin($code,$val,'log_'.$this->id.'.'.$ext);
 					} else{
-						$this->dbg_addin($code,$val,'log_system.'.$ext,'debug');	
+						$this->dbg_addin($code,$val,'log_system.'.$ext);	
 					}
 				}
 			}
@@ -85,14 +233,15 @@ trait DebugB{
 		$this->retval=TRUE;
 	}
 	function ot_log($in,$from,$parameters,$ret){		
+		$this->dbg_addin($from,array('in'=>str_replace('::'.$from,'',$in ),'parameters'=>count ($parameters)),"OnTime_Development.tas","help");
 		if (($this->err!="0") and ($this->SaveError)) {
 			$val =  array('kind'=>'Error','Code'=>$this->err,'Code'=>$this->id,'in'=>$in,'from'=>$from,'param'=>$parameters, "return"=>$ret);
 			$code = uniqid($this->actses , true );			
 			$ext = date("Ymd");
 				if ($this->DebugFor=='user'){
-					$this->dbg_addin($code,$val,'log_error'.$this->id.'.'.$ext,'debug');
+					$this->dbg_addin($code,$val,'log_error'.$this->id.'.'.$ext);
 				} else{
-					$this->dbg_addin($code,$val,'log_error.'.$ext,'debug');	
+					$this->dbg_addin($code,$val,'log_error.'.$ext);	
 				}
 		}
 		
@@ -109,31 +258,24 @@ trait DebugB{
 			if ($this->DebugTo=='disk'){
 				$ext = date("Ymd");
 				if ($this->DebugFor=='user'){
-					$this->dbg_addin($code,$val,'log_'.$this->id.'.'.$ext,'debug');
+					$this->dbg_addin($code,$val,'log_'.$this->id.'.'.$ext);
 				} else{
-					$this->dbg_addin($code,$val,'log_system.'.$ext,'debug');	
+					$this->dbg_addin($code,$val,'log_system.'.$ext);	
 				}
 			}
 		}
 		return(TRUE);
 	}
-	protected function dbg_addin($key, $value, $file, $inside="no"){
-		$data = $this->dbg_readif($file, $inside);
-		if (array_key_exists($key,$data)) {
-			$this->err='C0010M007';
-		} else {
-			$data[$key]=$value;
-			$this->dbg_write($file,$data,$inside);
-			$this->retval=TRUE;
-		}
+	protected function dbg_addin($key, $value, $file, $in ='debug')
+	{
+		$data = $this->dbg_readif($file, $in);
+		$data[$key]=$value;
+		$this->dbg_write($file,$data, $in);
 		return $data;
 	}	
-	protected function dbg_readif($file, $inside='no'){
-		if ($inside=='no') {
-			$file=$this->container.'/'.$file;
-		} else {
-			$file=$this->container.'/'.$inside. '/'.$file;
-		}
+	protected function dbg_readif($file, $in ='debug')
+	{
+		$file=$this->container.'/'.$in.'/'.$file;
 		$aread=[];
 		if (file_exists($file)) {
 			$stream=fopen($file,"r");
@@ -144,32 +286,20 @@ trait DebugB{
 				}
 				$aread=json_decode($vread,true);
 				fclose($stream);
-			} else {
-				$this->err='C0010M001';
 			}
 		}
 		return $aread;
 	}	
-	protected function dbg_write($file, $data, $inside="no"){
-		if ($inside=='no') {
-			$file=$this->container.'/'.$file;
-		} else {
-			$file=$this->container.'/'.$inside. '/'.$file;
-		}
-		$this->err='0';
+	protected function dbg_write($file, $data, $in ='debug')
+	{
+		$file=$this->container.'/'.$in.'/'.$file;
 		$stream=fopen($file, "w");
 		if ($stream) {
 			$save=fwrite($stream,json_encode($data,JSON_UNESCAPED_SLASHES));
 			if ($save) {
-				$this->retval=FALSE;
 				fclose($stream);
-			} else {
-				$this->err='C0010M003';
 			}
-		} else {
-			$this->err='C0010M002';
-			$this->errtext['C0010M002']='Failing create content';
 		}
-		return $this->err;
+		return TRUE;
 	}	
 }
